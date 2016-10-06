bb853e80a1dca6beab43dc90261948bf4064b296
Ranu Singhal <ranu1996@gmail.com>
2016-10-06 23:56:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName',function(req,res) {
     //articleName==article-one
     //articles[articlesName]==content object for article one
-    var articleName=req.params.articlrName;
+    var articleName=req.params.articleName;
    res.send(createTemplate(articles[articlesName]));
 });
 
